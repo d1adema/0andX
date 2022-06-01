000124103c0cacbd3c41a051f0cfacf8dfe8983d
d1adema <dmitriycontrast@gmail.com>
2022-06-01 23:50:29 +0300

    Added draw

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 const box = document.querySelectorAll(".box"),
-  boxArea = document.querySelector(".wrapper-area"),
-  wrapperResult = document.querySelector(".wrapper-result"),
-  contentResult = document.querySelector("#content"),
-  closeResult = document.querySelector("#btn-close"),
-  overlayResult = document.querySelector("#overlay");
+      boxArea = document.querySelector(".wrapper-area"),
+      wrapperResult = document.querySelector(".wrapper-result"),
+      contentResult = document.querySelector("#content"),
+      closeResult = document.querySelector("#btn-close"),
+      overlayResult = document.querySelector("#overlay"),
+      windowResult = document.querySelector('#modal-window'),
+      body = document.querySelector('body'),
+      switchBtn = document.querySelector('.switch-btn'),
+      h1 = document.querySelector('h1');
 let move = 0,
-  result = "";
+    result = "",
+    resultWord = "";
 
 boxArea.addEventListener("click", (e) => {
   if (e.target.className === "box") {
-    console.log(e.target);
     move % 2 === 0 ? (e.target.innerHTML = "X") : (e.target.innerHTML = "0");
     move++;
     checkResult();
@@ -34,20 +38,26 @@ function checkResult() {
       boxes[arrOfWin[i][1]].innerHTML === "X" &&
       boxes[arrOfWin[i][2]].innerHTML === "X"
     ) {
+      resultWord = 'Победили '
       result = "крестики";
-      showModal(result);
+      showModal(resultWord, result);
     } else if (
       boxes[arrOfWin[i][0]].innerHTML === "0" &&
       boxes[arrOfWin[i][1]].innerHTML === "0" &&
       boxes[arrOfWin[i][2]].innerHTML === "0"
     ) {
+      resultWord = 'Победили '
       result = "нолики";
-      showModal(result);
+      showModal(resultWord, result);
+    } else if (move === 9) {
+      resultWord = 'Победила '
+      result = 'дружба';
+      showModal(resultWord, result);
     }
   }
 }
 
-function showModal(winner) {
+function showModal(win, winner) {
   wrapperResult.style = "display: block;";
   wrapperResult.addEventListener("click", (e) => {
     if (e.target === closeResult || e.target === overlayResult) {
@@ -55,5 +65,5 @@ function showModal(winner) {
       location.reload();
     }
   });
-  contentResult.innerHTML = `Победили ${winner}`;
+  contentResult.innerHTML = `${win} ${winner}`;
 }
